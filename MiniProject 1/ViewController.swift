e3958abf9b915c88784bd699730ddc29dda62f06
fdcbeaver <fdc.johnbeaver@gmail.com>
2026-09-08 11:36:51 +0800

    Initial Commit

---
+++ MiniProject 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniProject 1
+//
+//  Created by FDC.Beaver-NC-IOS on 9/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
